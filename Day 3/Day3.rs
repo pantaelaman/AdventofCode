22f09254b9ce35a0d1e469aa8b9b103541e37f03
pantaelaman <calvin@theophoffs.com>
2021-12-04 18:16:25 -0500

    Day 3 part 2 complete

--- Day 3/Day3.rs
+++ Day 3/Day3.rs
@@ -2,10 +2,12 @@ use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
 fn main() {
-    let (gamma, epsilon) = process_data(process_input("Day3Input.txt"));
-    println!("Gamma: {}", gamma);
-    println!("Epsilon: {}", epsilon);
-    println!("Answer: {}", gamma*epsilon);
+    let data = process_input("Day3Input.txt");
+    let oxygen = process_oxygen_data(data.clone());
+    let co2 = process_co2_data(data.clone());
+    println!("Oxygen: {}", oxygen);
+    println!("CO2: {}", co2);
+    println!("Answer: {}", oxygen*co2);
 }
 
 fn process_input(file: &str) -> Vec<Vec<u8>> {
@@ -25,7 +27,7 @@ fn process_input(file: &str) -> Vec<Vec<u8>> {
     return ret;
 }
 
-fn process_data(data: Vec<Vec<u8>>) -> (u32, u32) {
+fn count_majority(data: Vec<Vec<u8>>) -> Vec<u8> {
     let mut ret: Vec<u8> = Vec::new();
     let mut count: Vec<i32> = vec![0; data.last().expect("Something went wrong.").len()];
 
@@ -38,14 +40,19 @@ fn process_data(data: Vec<Vec<u8>>) -> (u32, u32) {
     }
 
     for (i,value) in count.iter().enumerate() {
-        if *value as usize > data.len()/2 {ret.push(1)}
+        if *value as f32 >= (data.len() as f32)/2.0 {ret.push(1)}
         else {ret.push(0)}
     }
+
+    return ret;
+}
+
+fn process_data(data: Vec<Vec<u8>>) -> (u32, u32) {
+    let mut ret: Vec<u8> = count_majority(data.clone());
 
     let mut proc: Vec<u32> = vec![0; data.last().expect("Something went wrong.").len()];
     for (i,value) in ret.iter().enumerate() {
         proc[i] = (*value as u32) << ret.len()-1-i;
-        println!("{}", proc[i]);
     }
     let mut gamma: u32 = 0b00;
     for (i,value) in proc.iter().enumerate() {
@@ -57,4 +64,72 @@ fn process_data(data: Vec<Vec<u8>>) -> (u32, u32) {
     epsilon = u32::from_str_radix(proc.as_str(), 2).expect("Could not produce epsilon.");
 
     return (gamma, epsilon);
+}
+
+fn process_oxygen_data(data: Vec<Vec<u8>>) -> u32 {
+    println!("-- Oxygen --");
+    let mut mod_data = data.clone();
+
+    for (i,_) in data.last().expect("Something went wrong.").iter().enumerate() {
+        let majority = count_majority(mod_data.clone());
+        println!("Round {}: {:#?}", i, mod_data);
+        println!("Majority {}: {}", i, majority[i]);
+        let mut new_data: Vec<Vec<u8>> = Vec::new();
+        for (j,line) in mod_data.iter().enumerate() {
+            if line[i] == majority[i] {
+                new_data.push(line.clone());
+            }
+        }
+        mod_data = new_data;
+        if mod_data.len() == 1 {break}
+    }
+
+    let ret: Vec<u8> = (&mod_data[0]).clone();
+
+    let mut proc: Vec<u32> = vec![0; data.last().expect("Something went wrong.").len()];
+    for (i,value) in ret.iter().enumerate() {
+        proc[i] = (*value as u32) << ret.len()-1-i;
+        println!("V: {}, P: {}", value, proc[i]);
+    }
+
+    let mut oxygen: u32 = 0b00;
+    for (i,value) in proc.iter().enumerate() {
+        oxygen = oxygen | (*value as u32);
+    }
+
+    return oxygen;
+}
+
+fn process_co2_data(data: Vec<Vec<u8>>) -> u32 {
+    println!("-- CO2 --");
+    let mut mod_data = data.clone();
+
+    for (i,_) in data.last().expect("Something went wrong.").iter().enumerate() {
+        let majority = count_majority(mod_data.clone());
+        println!("Round {}: {:#?}", i, mod_data);
+        println!("Majority {}: {}", i, majority[i]);
+        let mut new_data: Vec<Vec<u8>> = Vec::new();
+        for (j,line) in mod_data.iter().enumerate() {
+            if line[i] != majority[i] {
+                new_data.push(line.clone());
+            }
+        }
+        mod_data = new_data;
+        if mod_data.len() == 1 {break}
+    }
+
+    let ret: Vec<u8> = (&mod_data[0]).clone();
+
+    let mut proc: Vec<u32> = vec![0; data.last().expect("Something went wrong.").len()];
+    for (i,value) in ret.iter().enumerate() {
+        proc[i] = (*value as u32) << ret.len()-1-i;
+        println!("V: {}, P: {}", value, proc[i]);
+    }
+
+    let mut co2: u32 = 0b00;
+    for (i,value) in proc.iter().enumerate() {
+        co2 = co2 | (*value as u32);
+    }
+
+    return co2;
 }
