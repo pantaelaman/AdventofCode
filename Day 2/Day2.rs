034fe0d8c78de7bf8a4dba4dfb060f5288e90580
pantaelaman <calvin@theophoffs.com>
2021-12-10 21:42:03 -0500

    Day 2 part 2 complete

--- Day 2/Day2.rs
+++ Day 2/Day2.rs
@@ -2,23 +2,24 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::iter::Iterator;
 
+#[derive (Clone, Debug)]
 struct Program {
     vec: Vec<i32>,
-    pos: usize,
+    ptr: usize,
 }
 
 impl Program {
     pub fn new(vec: Vec<i32>) -> Program {
-        Program{vec, pos: 0}
+        Program{vec, ptr: 0}
     }
 
     pub fn next(&mut self) -> i32 {
-        self.pos += 1;
-        if self.pos >= self.vec.len() {
-            self.pos = 0;
+        self.ptr += 1;
+        if self.ptr >= self.vec.len() {
+            self.ptr = 0;
             return *self.vec.last().unwrap();
         }
-        self.vec[self.pos - 1]
+        self.vec[self.ptr - 1]
     }
 
     pub fn read(&self, addr: i32) -> i32 {
@@ -61,7 +62,21 @@ fn process_input(file: &str) {
     let reader = BufReader::new(file);
 
     let vec_prog = reader.lines().nth(0).unwrap().expect("Could not read line 0").split(",").map(|el| el.parse::<i32>().expect("Bad format in program")).collect::<Vec<i32>>();
-    let mut program = Program::new(vec_prog);
-    program.run();
-    println!("Final: {:?}", program.vec);
+    let program = Program::new(vec_prog);
+    println!("Final: {:?}", try_values(&program, 19690720));
+}
+
+fn try_values(program: &Program, look_for: i32) -> i32 {
+    for n in 0..100 {
+        for v in 0..100 {
+            let mut current = program.clone();
+            current.write(1, n);
+            current.write(2, v);
+            current.run();
+            if current.read(0) == look_for {
+                return (100 * n) + v;
+            }
+        }
+    }
+    return -1;
 }
