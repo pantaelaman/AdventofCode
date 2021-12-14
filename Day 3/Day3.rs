75da0ce0c9ea5c47cbeeb446d56cdb38aae1341e
pantaelaman <calvin@theophoffs.com>
2021-12-13 21:27:32 -0500

    Day 3 in progress

--- Day 3/Day3.rs
+++ Day 3/Day3.rs
@@ -1,45 +1,75 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::collections::HashSet;
 
 fn main() {
-    process_input("Day3Test1.txt");
+    process_input("Day3Input.txt");
 }
 
 fn process_input(file: &str) {
-    let file = File::open(file).expect("Bad path");
+    let file = File::open(file).expect(format!("Bad path {}", file).as_str());
     let reader = BufReader::new(file);
 
     let mut lines = reader.lines().collect::<Vec<Result<String, std::io::Error>>>();
     let mut lines = lines.iter_mut();
 
-    let line1 = lines.nth(0).unwrap().as_ref().expect("Could not read line 0").split(",").scan((0_i32,0_i32),|pos,el| {
-        let vertical = match el.chars().nth(0).unwrap() {
-            'U' => true,
-            'D' => true,
-            'L' => false,
-            'R' => false,
-            _ => panic!("Something went wrong"),
-        };
-        let amplitude = el.chars().skip(1).collect::<String>();
-        println!("{}", amplitude);
-        let amplitude = amplitude.parse::<i32>().expect("Could not parse amplitude");
-        let mut ret: Vec<(i32,i32)> = Vec::new();
-        if vertical {
-            for y in (pos.1)..(pos.1 + amplitude + 1) {
-                ret.push((pos.0,y));
-            }
-            pos.1 = pos.1 + amplitude;
-        } else {
-            for x in (pos.0)..(pos.0 + amplitude + 1) {
-                ret.push((x,pos.1))
-            }
-            pos.0 = pos.0 + amplitude;
-        }
-        Some(ret)
-    }).fold(Vec::new(), |mut acc,mut el| {
+    let mut line1 = lines.nth(0).unwrap().as_ref().expect("Could not read line 0").split(",").scan((0_i32,0_i32),|pos,el| process_line(pos, el)).fold(Vec::new(), |mut acc,mut el| {
         acc.append(&mut el);
         acc
     });
-    println!("{:?}", line1);
-    let line2 = lines.nth(0).unwrap().as_ref().expect("Could not read line 1");
+    let mut line2 = lines.nth(0).unwrap().as_ref().expect("Could not read line 1").split(",").scan((0_i32,0_i32),|pos,el| process_line(pos, el)).fold(Vec::new(), |mut acc,mut el| {
+        acc.append(&mut el);
+        acc
+    });
+
+    line1.sort();
+    line2.sort();
+    
+    let mut count = 0;
+    let mut total = line1.len();
+    'main: for i in line1 {
+        for j in &line2 {
+            if i == *j {
+                println!("Distance: {}", i.0.abs() + i.1.abs());
+                break 'main;
+            }
+            if j.0 > i.0 || j.1 > i.1 {break}
+        }
+        println!("Checked major {} of {}", count, total);
+        count += 1;
+    }
+}
+
+fn process_line(pos: &mut (i32,i32), el: &str) -> Option<Vec<(i32,i32)>> {
+    let mut points: Vec<(i32,i32)> = Vec::new();
+    let amplitude = (match el.chars().nth(0).unwrap() {
+        'L' | 'R' => false,
+        'U' | 'D' => true,
+        _ => panic!("Unexpected character"),
+    }, el[1..].parse::<i32>().unwrap(), match el.chars().nth(0).unwrap() {
+        'L' | 'D' => -1,
+        'R' | 'U' => 1,
+        _ => panic!("Unexpected character"),
+    });
+    if amplitude.0 {
+        for n in 0..(amplitude.1) {
+            points.push((pos.0, pos.1 + (n * amplitude.2)));
+        }
+        pos.1 += amplitude.1 * amplitude.2;
+    } else {
+        for n in 0..(amplitude.1) {
+            points.push((pos.0 + (n * amplitude.2), pos.1));
+        }
+        pos.0 += amplitude.1 * amplitude.2;
+    }
+    return Some(points);
+}
+
+fn calculate_shortest_distance(intersections: &HashSet<(i32,i32)>) -> i32 {
+    let mut min = i32::MAX;
+    for intersection in intersections {
+        let distance = intersection.0.abs() + intersection.1.abs();
+        if distance < min {min = distance;}
+    }
+    return min;
 }
