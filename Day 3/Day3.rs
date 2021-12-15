3b883147ee9a19808fc541fcac30851811c1d8b3
pantaelaman <calvin@theophoffs.com>
2021-12-15 07:42:55 -0500

    Day 3 in progress

--- Day 3/Day3.rs
+++ Day 3/Day3.rs
@@ -13,63 +13,19 @@ fn process_input(file: &str) {
     let mut lines = reader.lines().collect::<Vec<Result<String, std::io::Error>>>();
     let mut lines = lines.iter_mut();
 
-    let mut line1 = lines.nth(0).unwrap().as_ref().expect("Could not read line 0").split(",").scan((0_i32,0_i32),|pos,el| process_line(pos, el)).fold(Vec::new(), |mut acc,mut el| {
-        acc.append(&mut el);
-        acc
-    });
-    let mut line2 = lines.nth(0).unwrap().as_ref().expect("Could not read line 1").split(",").scan((0_i32,0_i32),|pos,el| process_line(pos, el)).fold(Vec::new(), |mut acc,mut el| {
-        acc.append(&mut el);
-        acc
-    });
-
-    line1.sort();
-    line2.sort();
+    let mut line1 = lines.nth(0).unwrap().as_ref().expect("Could not read line 0").split(",").scan((0_i32,0_i32),|pos,el| process_line(pos, el));
+    let mut line2 = lines.nth(0).unwrap().as_ref().expect("Could not read line 1").split(",").scan((0_i32,0_i32),|pos,el| process_line(pos, el));
     
-    let mut count = 0;
-    let mut total = line1.len();
-    'main: for i in line1 {
-        for j in &line2 {
-            if i == *j {
-                println!("Distance: {}", i.0.abs() + i.1.abs());
-                break 'main;
-            }
-            if j.0 > i.0 || j.1 > i.1 {break}
-        }
-        println!("Checked major {} of {}", count, total);
-        count += 1;
-    }
+    println!("{:#?}", line1);
 }
 
-fn process_line(pos: &mut (i32,i32), el: &str) -> Option<Vec<(i32,i32)>> {
-    let mut points: Vec<(i32,i32)> = Vec::new();
-    let amplitude = (match el.chars().nth(0).unwrap() {
-        'L' | 'R' => false,
-        'U' | 'D' => true,
-        _ => panic!("Unexpected character"),
-    }, el[1..].parse::<i32>().unwrap(), match el.chars().nth(0).unwrap() {
-        'L' | 'D' => -1,
-        'R' | 'U' => 1,
-        _ => panic!("Unexpected character"),
-    });
-    if amplitude.0 {
-        for n in 0..(amplitude.1) {
-            points.push((pos.0, pos.1 + (n * amplitude.2)));
-        }
-        pos.1 += amplitude.1 * amplitude.2;
-    } else {
-        for n in 0..(amplitude.1) {
-            points.push((pos.0 + (n * amplitude.2), pos.1));
-        }
-        pos.0 += amplitude.1 * amplitude.2;
+fn process_line(pos: &mut (i32,i32), el: &str) -> Option<((i32,i32),(i32,i32))> {
+    let value: i32 = el[1..].parse().unwrap();
+    let curpos = pos.clone();
+    match el.chars().nth(0).unwrap() {
+        'U' => {*pos = (pos.0,pos.1-value); return Some((curpos,pos.clone()));},
+        'D' => {*pos = (pos.0,pos.1+value); return Some((curpos,pos.clone()));},
+        'L' => {*pos = (pos.0-value,pos.1); return Some((curpos,pos.clone()));},
+        'R' => {*pos = (pos.0+value,pos.1); return Some((curpos,pos.clone()));},
     }
-    return Some(points);
-}
-
-fn calculate_shortest_distance(intersections: &HashSet<(i32,i32)>) -> i32 {
-    let mut min = i32::MAX;
-    for intersection in intersections {
-        let distance = intersection.0.abs() + intersection.1.abs();
-        if distance < min {min = distance;}
-    }
-    return min;
 }
