dffb7d5cab97259d044b18b1df6adbc68f1c72ab
pantaelaman <calvin@theophoffs.com>
2021-12-16 10:32:54 -0500

    Day 15 incomplete

--- Day 15/Day15.rs
+++ Day 15/Day15.rs
@@ -3,141 +3,101 @@ use std::io::{BufRead,BufReader};
 use std::collections::{BinaryHeap,HashMap};
 use std::cmp::Reverse;
 
-struct HeapHolder<T, F: Ord> {
-    priority: F,
-    held: T,
+struct Neighbours {
+    position: (usize,usize),
+    size: usize,
+    index: usize,
 }
 
-impl<T,F: Ord> HeapHolder<T,F> {
-    fn new(held: T, priority: F) -> Self {
-        HeapHolder{priority,held}
+impl Neighbours {
+    const OFFSETS: [(i32,i32); 4] = [(1,0),(-1,0),(0,1),(0,-1)];
+
+    fn new(position: (usize,usize), size: usize) -> Neighbours {
+        Neighbours{position,size,index: 0}
     }
 }
 
-impl<T,F: Ord> HeapHolder<T,F> {
-    fn get(&self) -> &T {
-        &self.held
-    }
-    fn get_mut(&mut self) -> &mut T {
-        &mut self.held
-    }
-    fn get_priority(&self) -> &F {
-        &self.priority
-    }
-}
-
-impl<T,F: Ord> PartialEq for HeapHolder<T,F> {
-    fn eq(&self, other: &Self) -> bool {
-        self.priority == other.priority
-    }
-}
-
-impl<T,F: Ord> Eq for HeapHolder<T,F> {}
-
-impl<T,F: Ord> PartialOrd for HeapHolder<T,F> {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.priority.cmp(&other.priority))
+// thanks u/gosslot
+impl std::iter::Iterator for Neighbours {
+    type Item = (usize,usize);
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.index < 4 {
+            let offset = Self::OFFSETS[self.index];
+            self.index += 1;
+            let dx = self.position.0 as i32 + offset.0;
+            let dy = self.position.1 as i32 + offset.1;
+            if dx >= 0 && (dx as usize) < self.size && dy >= 0 && (dy as usize) < self.size {
+                return Some((dx as usize, dy as usize));
+            }
+        }
+        None
     }
 }
 
-impl<T,F: Ord> Ord for HeapHolder<T,F> {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.priority.cmp(&other.priority)
-    }
+fn main () {
+    process_input("Day15Test.txt");
 }
 
-fn main() {
-    process_input("Day15Input.txt");
-}
-
-fn process_input(filepath: &str) {
-    let file = File::open(filepath).expect("Bad path");
-    let mut map: Vec<Vec<(i32,(usize,usize),Vec<(usize,usize)>)>> = Vec::new();
+fn process_input(file: &str) {
+    let file = File::open(file).expect("Bad path");
     let reader = BufReader::new(file);
-    let counter = BufReader::new(File::open(filepath).unwrap());
-    let linecount = counter.lines().count();
+    let lines: Vec<String> = reader.lines().scan(0, |acc, el| {
+        let result = Some(el.expect(format!("Could not read line {}", acc).as_str()));
+        *acc += 1;
+        result
+    }).collect();
 
-    for (i,line) in reader.lines().enumerate() {
-        let row_neighbors: Vec<usize>;
-        if i == 0 {
-            row_neighbors = vec![i+1];
-        } else if i == linecount-1 {
-            row_neighbors = vec![i-1];
-        } else {
-            row_neighbors = vec![i-1,i+1];
-        }
-        let line = line.expect(format!("Could not read line {}", i).as_str());
-        let charcount = line.len();
-        let mut row: Vec<(i32,(usize,usize),Vec<(usize,usize)>)> = Vec::new();
-        for (j,chr) in line.chars().enumerate() {
-            let col_neighbors: Vec<usize>;
-            if j == 0 {
-                col_neighbors = vec![j+1];
-            } else if j == charcount-1 {
-                col_neighbors = vec![j-1];
-            } else {
-                col_neighbors = vec![j-1,j+1];
-            }
-            let mut neighbors: Vec<(usize,usize)> = Vec::new();
-            for k in &row_neighbors {
-                neighbors.push((*k,j));
-            }
-            for k in &col_neighbors {
-                neighbors.push((i,*k));
-            }
-            let main: i32 = chr.to_string().parse().unwrap();
-            let index = (i,j);
-            row.push((main,index,neighbors));
-        }
-        map.push(row);
+    let size = lines.len();
+    let mut map = Vec::with_capacity(size);
+    for (index,line) in lines.iter().enumerate() {
+        map.push(line.chars().scan(0, |acc, el| {
+            let result = Some(el.to_string().parse::<i32>().expect(format!("Could not read char {} on line {}",acc,index).as_str()));
+            *acc += 1;
+            result
+        }).collect::<Vec<i32>>());
     }
-    let (cost,path) = dijkstra(&map);
-    println!("{:?}\nCost: {}", path,cost);
+    let expanded = expand_map(map.clone());
+    let size = expanded.len();
+    let cost = dijkstra(&map, (0,0), (size - 1, size - 1));
+    println!("Cost: {}", cost);
 }
 
-fn dijkstra(map: &Vec<Vec<(i32,(usize,usize),Vec<(usize,usize)>)>>) -> (i32,Vec<(usize,usize)>) {
-    let mut queue: BinaryHeap<HeapHolder<&(i32,(usize,usize),Vec<(usize,usize)>),Reverse<i32>>> = BinaryHeap::new();
-    queue.push(HeapHolder::new(&map[0][0], Reverse(0)));
-    let mut cur_cost: HashMap<(usize,usize),i32> = HashMap::new();
-    cur_cost.insert((0,0),0);
-    let mut came_from: HashMap<(usize,usize),Option<(usize,usize)>> = HashMap::new();
-    came_from.insert((0,0),None);
-    let goal = (map.len()-1,map[0].len()-1);
-
-    while !queue.is_empty() {
-        let raw_current = queue.pop().unwrap();
-        let current = raw_current.get();
-
-        if current.1 == goal {break}
-
-        for nextindex in &current.2 {
-            let next = &map[nextindex.0][nextindex.1];
-            let new_cost = cur_cost.get(&current.1).unwrap() + next.0;
-            if match cur_cost.get(&next.1) {
-                Some(val) => new_cost < *val,
-                None => true,
-            } {
-                *cur_cost.entry(next.1).or_insert(0) = new_cost;
-                queue.push(HeapHolder::new(next,Reverse(new_cost)));
+fn expand_map(map: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    let size = map.len();
+    let mut result = vec![vec![0; size * 5]; size * 5];
+    for row_offset in 0..5 {
+        for (r, row) in map.iter().enumerate() {
+            for col_offset in 0..5 {
+                for (c, &item) in row.iter().enumerate() {
+                    let new = (item + col_offset + row_offset) % 10;
+                    result[row_offset as usize * size + r][col_offset as usize * size + c] = new;
+                }
             }
-            came_from.insert(*nextindex, Some(current.1));
+        }
+    }
+    result
+}
+
+fn dijkstra(map: &Vec<Vec<i32>>, start: (usize,usize), goal: (usize,usize)) -> i32 {
+    let size = map.len();
+    let mut costs: HashMap<(usize,usize),i32> = HashMap::new();
+    let mut previous: HashMap<(usize,usize),Option<(usize,usize)>> = HashMap::new();
+    costs.insert(start, 0);
+    previous.insert(start, None);
+    let mut queue: BinaryHeap<Reverse<(i32,(usize,usize))>> = BinaryHeap::new();
+    queue.push(Reverse((0,start)));
+
+    while let Some(Reverse((cost,pos))) = queue.pop() {
+        let neighbours = Neighbours::new(pos,size);
+        for neighbour in neighbours {
+            let new_cost = cost + map[neighbour.1][neighbour.0];
+            if costs.get(&neighbour).map_or(true, |old| new_cost < *old) {
+                queue.push(Reverse((new_cost, neighbour)));
+                costs.insert(neighbour, new_cost);
+                previous.insert(neighbour, Some(pos));
+            }
         }
     }
 
-    println!("{:?}", came_from.get(&(0,0)));
-
-    let mut path: Vec<(usize,usize)> = Vec::new();
-    let mut active = goal;
-    /* loop {
-        path.push(active);
-        match came_from.get(&active).unwrap() {
-            Some(loc) => active = *loc,
-            None => break,
-        };
-    } */
-
-    match cur_cost.get(&goal) {
-        Some(val) => (*val,path),
-        None => (-1,path),
-    }
+    *costs.get(&goal).unwrap_or(&-1)
 }
---
+++ Day 15/Day15Other.rs
@@ -0,0 +1,162 @@
+// https://github.com/Fadi88/AoC/blob/master/2021/day15/main.rs
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
+use std::collections::HashMap;
+use std::time;
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+struct Point {
+    cost: u16,
+    pos: (u16, u16),
+}
+
+impl Ord for Point {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other
+            .cost
+            .cmp(&self.cost)
+            .then_with(|| self.pos.cmp(&other.pos))
+    }
+}
+
+impl PartialOrd for Point {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn bench(f: fn()) {
+    let t0 = time::Instant::now();
+    let ret = f();
+    println!("time used {:?}", time::Instant::now().duration_since(t0));
+
+    ret
+}
+
+fn part_1() {
+    let mut grid: Vec<Vec<u8>> = Vec::new();
+    for l in include_str!("Day15Input.txt").split("\n") {
+        if l.len() < 3 {
+            continue;
+        }
+        grid.push(
+            l.chars()
+                .map(|x| x.to_digit(10).unwrap() as u8)
+                .collect::<Vec<u8>>(),
+        );
+    }
+
+    let deltas: [(i16, i16); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+
+    let mut visit: BinaryHeap<Point> = BinaryHeap::new();
+
+    visit.push(Point {
+        cost: 0,
+        pos: (0, 0),
+    });
+
+    let mut risk: HashMap<(i16, i16), u16> = HashMap::new();
+    risk.insert((0, 0), 0);
+
+    while let Some(Point { cost, pos }) = visit.pop() {
+        for (dx, dy) in deltas {
+            if pos.0 as i16 + dx >= 0
+                && pos.0 as i16 + dx < grid.len() as i16
+                && pos.1 as i16 + dy >= 0
+                && pos.1 as i16 + dy < grid[0].len() as i16
+            {
+                let new_risk =
+                    cost + grid[(pos.0 as i16 + dx) as usize][(pos.1 as i16 + dy) as usize] as u16;
+                if *risk
+                    .entry(((pos.0 as i16 + dx), (pos.1 as i16 + dy)))
+                    .or_insert(u16::MAX)
+                    > new_risk
+                {
+                    risk.insert(((pos.0 as i16 + dx), (pos.1 as i16 + dy)), new_risk);
+                    visit.push(Point {
+                        cost: new_risk,
+                        pos: ((pos.0 as i16 + dx) as u16, (pos.1 as i16 + dy) as u16),
+                    });
+                }
+            }
+        }
+    }
+
+    println!(
+        "{}",
+        risk.get(&((grid.len() - 1) as i16, (grid[0].len() - 1) as i16))
+            .unwrap()
+    );
+}
+
+fn get_risk(grid: &Vec<Vec<u8>>, x: u16, y: u16) -> u8 {
+    let tmp = grid[x as usize % grid.len()][y as usize % grid[0].len()] as u16
+        + (x / grid.len() as u16)
+        + (y / grid[0].len() as u16);
+    (tmp as u8 - 1) % 9 + 1
+}
+
+fn part_2() {
+    let mut grid: Vec<Vec<u8>> = Vec::new();
+    for l in include_str!("Day15Input.txt").split('\n').take(100) {
+        if l.len() < 3 {
+            continue;
+        }
+        grid.push(
+            l.chars()
+                .map(|x| x.to_digit(10).unwrap() as u8)
+                .collect::<Vec<u8>>(),
+        );
+    }
+
+    let deltas: [(i16, i16); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+
+    let mut visit: BinaryHeap<Point> = BinaryHeap::new();
+
+    visit.push(Point {
+        cost: 0,
+        pos: (0, 0),
+    });
+
+    let mut risk: HashMap<(i16, i16), u16> = HashMap::new();
+    risk.insert((0, 0), 0);
+
+    while let Some(Point { cost, pos }) = visit.pop() {
+        for (dx, dy) in deltas {
+            if pos.0 as i16 + dx >= 0
+                && pos.0 as i16 + dx < 5 * grid.len() as i16
+                && pos.1 as i16 + dy >= 0
+                && pos.1 as i16 + dy < 5 * grid[0].len() as i16
+            {
+                let new_risk = cost
+                    + get_risk(
+                        &grid,
+                        (pos.0 as i16 + dx) as u16,
+                        (pos.1 as i16 + dy) as u16,
+                    ) as u16;
+                if *risk
+                    .entry(((pos.0 as i16 + dx), (pos.1 as i16 + dy)))
+                    .or_insert(u16::MAX)
+                    > new_risk
+                {
+                    risk.insert(((pos.0 as i16 + dx), (pos.1 as i16 + dy)), new_risk);
+                    visit.push(Point {
+                        cost: new_risk,
+                        pos: ((pos.0 as i16 + dx) as u16, (pos.1 as i16 + dy) as u16),
+                    });
+                }
+            }
+        }
+    }
+
+    println!(
+        "{}",
+        risk.get(&((5 * grid.len() - 1) as i16, (5 * grid[0].len() - 1) as i16))
+            .unwrap()
+    );
+}
+
+fn main() {
+    bench(part_1);
+    bench(part_2);
+}
