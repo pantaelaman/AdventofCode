601f4bb877a955c2191d93f35211912a3853cb38
pantaelaman <calvin@theophoffs.com>
2021-12-17 13:21:49 -0500

    Day 16 part 2 incomplete

--- Day 16/Day16.rs
+++ Day 16/Day16.rs
@@ -25,6 +25,73 @@ impl Packet {
             },
         }
     }
+
+    fn run_packet(&self) -> u64 {
+        match self {
+            Packet::Literal(_, value) => {
+                // Literal value
+                *value
+            },
+            Packet::Operator(_, ptype, packets) => {
+                match ptype {
+                    0 => {
+                        // Sum
+                        let mut result = 0_u64;
+                        for packet in packets {
+                            result += packet.run_packet();
+                        }
+                        result
+                    },
+                    1 => {
+                        // Product
+                        let mut result = packets[0].run_packet();
+                        for packet in &packets[1..] {
+                            result *= packet.run_packet();
+                        }
+                        result
+                    },
+                    2 => {
+                        // Minimum
+                        let mut result = u64::MAX;
+                        for packet in packets {
+                            let packet_result = packet.run_packet();
+                            if packet_result < result {
+                                result = packet_result;
+                            }
+                        }
+                        result
+                    },
+                    3 => {
+                        // Maximum
+                        let mut result = 0_u64;
+                        for packet in packets {
+                            let packet_result = packet.run_packet();
+                            if packet_result > result {
+                                result = packet_result;
+                            }
+                        }
+                        result
+                    },
+                    5 => {
+                        // Greater Than
+                        let result = if packets[0].run_packet() > packets[1].run_packet() {1} else {0};
+                        result
+                    },
+                    6 => {
+                        // Less Than
+                        let result = if packets[0].run_packet() < packets[1].run_packet() {1} else {0};
+                        result
+                    },
+                    7 => {
+                        // Equals
+                        let result = if packets[0].run_packet() == packets[1].run_packet() {1} else {0};
+                        result
+                    },
+                    _ => panic!("Unexpected type"),
+                }
+            }
+        }
+    }
 }
 
 fn main() {
@@ -36,32 +103,35 @@ fn process_input(file: &str) {
     let reader = BufReader::new(file);
     let raw = reader.lines().nth(0).unwrap().expect("Could not read code");
 
-    let mut coded: VecDeque<char> = raw.chars().fold(VecDeque::new(), |mut acc,chr| {
+    let mut code: VecDeque<char> = raw.chars().fold(VecDeque::new(), |mut acc,chr| {
         acc.push_back(chr);
         acc
     });
 
-    println!("{:?}", coded);
-
-    run_program(&mut coded);
+    run_program(&mut code);
 }
 
-fn run_program(encoded: &mut VecDeque<char>) {
+fn run_program(code: &mut VecDeque<char>) {
     let mut queue: Program = Program::new();
-    fill_queue(encoded, &mut queue);
+    fill_queue(code, &mut queue);
+    // Get the main packet
     let packet = parse_packet(&mut queue);
+    // Part 1
     println!("Version Count: {}", packet.count_versions());
+    // Part 2
+    println!("Result: {}", packet.run_packet());
 }
 
 fn parse_packet(queue: &mut Program) -> Packet {
+    // Pop the first 6 bits and parse them to get the version and packet type
     let version = parse_programmed(&[queue.pop_front().unwrap(),queue.pop_front().unwrap(),queue.pop_front().unwrap()]) as u8;
     let ptype = parse_programmed(&[queue.pop_front().unwrap(),queue.pop_front().unwrap(),queue.pop_front().unwrap()]) as u8;
-    println!("Version: {}", version);
-    println!("Type: {}", ptype);
     if ptype == 4 {
+        // Packet Type is a Literal value
         let mut result: u64 = 0b0000;
         let mut segments: Vec<u64> = Vec::new();
         loop {
+            // Add segments until you see a segment that begins with a 0
             if queue.pop_front().unwrap() {
                 segments.push(parse_programmed(&[queue.pop_front().unwrap(),queue.pop_front().unwrap(),queue.pop_front().unwrap(),queue.pop_front().unwrap()]) as u64);
             } else {
@@ -69,35 +139,39 @@ fn parse_packet(queue: &mut Program) -> Packet {
                 break;
             }
         }
-        let total = segments.len();
+
+        // Push the binary segments into a u64
         for (index,segment) in segments.iter().enumerate() {
-            let actual = segment << (total-index) * 4;
+            let actual = segment << index * 4;
             result = result | actual;
         }
         return Packet::Literal(version, result);
     } else {
+        // Packet Type is an operator
         if queue.pop_front().unwrap() {
+            // Length ID is 1, so the next 11 bits represent the number of subpackets which follow
             let mut to_process: Vec<bool> = Vec::new();
+            // Get the next eleven numbers
             for _ in 0..11 {
                 to_process.push(queue.pop_front().unwrap());
             }
             let expected = parse_programmed(&to_process);
-            println!("Count: {}", expected);
             let mut packets: Vec<Packet> = Vec::new();
             for _ in 0..(expected) {
                 packets.push(parse_packet(queue));
             }
             return Packet::Operator(version, ptype, packets);
         } else {
+            // Length ID is 0, so the next 15 bits represent the length (in bits) of the total of the subpackets which follow
             let mut to_process: Vec<bool> = Vec::new();
             for _ in 0..15 {
                 to_process.push(queue.pop_front().unwrap());
             }
             let expected = parse_programmed(&to_process);
-            println!("Bits: {}", expected);
             let orig_size = queue.len();
             let mut cur_size = queue.len();
             let mut packets: Vec<Packet> = Vec::new();
+            // While the total number of bits that the packets have consumed is not equal to the expected amount, keep reading packets
             while orig_size - cur_size != expected {
                 packets.push(parse_packet(queue));
                 cur_size = queue.len();
@@ -107,13 +181,15 @@ fn parse_packet(queue: &mut Program) -> Packet {
     }
 }
 
-fn fill_queue(original: &mut VecDeque<char>, into: &mut VecDeque<bool>) {
+fn fill_queue(original: &mut VecDeque<char>, into: &mut Program) {
+    // Drain a character array into a Program
     for chr in original {
         into.append(&mut parse_hexadecimal(*chr).into_iter().collect());
     }
 }
 
 fn parse_hexadecimal(hex: char) -> Vec<bool> {
+    // Parse a hexadecimal character into a vector of bools
     match hex {
         '0' => vec![false, false, false, false],
         '1' => vec![false, false, false, true],
@@ -136,6 +212,7 @@ fn parse_hexadecimal(hex: char) -> Vec<bool> {
 }
 
 fn parse_programmed(programmed: &[bool]) -> usize {
+    // Parse an array of booleans into a binary number
     let size = programmed.len();
     let mut result: usize = 0b0000;
     for (i,item) in programmed.iter().enumerate() {
