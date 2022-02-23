a60f010c2aa60142042b9dcddb98e69f4dd700fd
pantaelaman <calvin@theophoffs.com>
2022-02-23 17:52:19 -0500

    Day 1 part 1 complete

--- Day 1/Day1.rs
+++ Day 1/Day1.rs
@@ -1,30 +1,26 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead,BufReader};
 
 fn main() {
-    println!("{}", process_input("Day1Input.txt"));
+    println!("Test: {}", process_input("Day1test.txt"));
+    println!("Result: {}", process_input("Day1input.txt"));
 }
 
-fn process_input(file: &str) -> i64 {
+fn process_input(file: &str) -> i32 {
     let file = File::open(file).expect("Bad path");
-    let mut fuel: i64 = 0;
+    let mut ret: i32 = 0;
     let reader = BufReader::new(file);
 
     for (index,line) in reader.lines().enumerate() {
         let line = line.expect(format!("Could not read line {}", index).as_str());
-        let value = line.parse::<i64>().expect(format!("Could not parse line {}", index).as_str());
-        println!("-- New --");
-        let cost = calculate_fuel((value / 3) - 2);
-        println!("Cost: {}", cost);
-        fuel += cost;
+        for (cindex,chr) in line.chars().enumerate() {
+            match chr {
+                '(' => ret+=1,
+                ')' => ret-=1,
+                _ => panic!("Unexpected character in input file"),
+            }
+        }
     }
-
-    return fuel;
+    ret
 }
 
-fn calculate_fuel(fuel: i64) -> i64 {
-    println!("{}", fuel);
-    let add = (fuel / 3) - 2;
-    if add <= 0 {return fuel}
-    else {return fuel + calculate_fuel(add)}
-}
