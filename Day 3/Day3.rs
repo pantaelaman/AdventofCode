6aad66ef99e4ff6bc2143a648423d5f2ee84814f
pantaelaman <calvin@theophoffs.com>
2022-02-23 21:36:52 -0500

    Day 3 part 1 complete

--- Day 3/Day3.rs
+++ Day 3/Day3.rs
@@ -1,31 +1,31 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead,BufReader};
 use std::collections::HashSet;
 
 fn main() {
-    process_input("Day3Input.txt");
+    println!("Test: {}", process_input("Day3test.txt"));
+    println!("Result: {}", process_input("Day3input.txt"));
 }
 
-fn process_input(file: &str) {
-    let file = File::open(file).expect(format!("Bad path {}", file).as_str());
+fn process_input(file: &str) -> usize {
+    let file = File::open(file).expect("Bad path");
     let reader = BufReader::new(file);
+    let input = reader.lines().next().expect("Empty file").expect("Could not read line 1");
+    let mut houses: HashSet<(isize,isize)> = HashSet::new();
+    let mut position: (isize,isize) = (0,0);
 
-    let mut lines = reader.lines().collect::<Vec<Result<String, std::io::Error>>>();
-    let mut lines = lines.iter_mut();
+    houses.insert(position);
 
-    let mut line1 = lines.nth(0).unwrap().as_ref().expect("Could not read line 0").split(",").scan((0_i32,0_i32),|pos,el| process_line(pos, el));
-    let mut line2 = lines.nth(0).unwrap().as_ref().expect("Could not read line 1").split(",").scan((0_i32,0_i32),|pos,el| process_line(pos, el));
-    
-    println!("{:#?}", line1);
-}
-
-fn process_line(pos: &mut (i32,i32), el: &str) -> Option<((i32,i32),(i32,i32))> {
-    let value: i32 = el[1..].parse().unwrap();
-    let curpos = pos.clone();
-    match el.chars().nth(0).unwrap() {
-        'U' => {*pos = (pos.0,pos.1-value); return Some((curpos,pos.clone()));},
-        'D' => {*pos = (pos.0,pos.1+value); return Some((curpos,pos.clone()));},
-        'L' => {*pos = (pos.0-value,pos.1); return Some((curpos,pos.clone()));},
-        'R' => {*pos = (pos.0+value,pos.1); return Some((curpos,pos.clone()));},
+    for chr in input.chars() {
+        match chr {
+            '^' => position.1+=1,
+            'v' => position.1-=1,
+            '<' => position.0-=1,
+            '>' => position.0+=1,
+            _ => panic!("Unexpected character"),
+        }
+        houses.insert(position);
     }
+
+    houses.iter().count()
 }
