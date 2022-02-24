27a25b9aba3b783fb36ec4632b2fd54fa94837be
pantaelaman <calvin@theophoffs.com>
2022-02-23 21:21:24 -0500

    Day 2 part 1 complete

--- Day 2/Day2.rs
+++ Day 2/Day2.rs
@@ -1,82 +1,49 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader};
-use std::iter::Iterator;
+use std::io::{BufRead,BufReader};
+use std::cmp::min;
 
-#[derive (Clone, Debug)]
-struct Program {
-    vec: Vec<i32>,
-    ptr: usize,
-}
-
-impl Program {
-    pub fn new(vec: Vec<i32>) -> Program {
-        Program{vec, ptr: 0}
-    }
-
-    pub fn next(&mut self) -> i32 {
-        self.ptr += 1;
-        if self.ptr >= self.vec.len() {
-            self.ptr = 0;
-            return *self.vec.last().unwrap();
-        }
-        self.vec[self.ptr - 1]
-    }
-
-    pub fn read(&self, addr: i32) -> i32 {
-        self.vec[addr as usize]
-    }
-
-    pub fn write(&mut self, addr: i32, val: i32) {
-        self.vec[addr as usize] = val
-    }
-
-    fn run(&mut self) {
-        match self.next() {
-            1 => {
-                let p1 = self.next();
-                let p2 = self.next();
-                let o1 = self.next();
-                self.write(o1,self.read(p1) + self.read(p2));
-            },
-            2 => {
-                let p1 = self.next();
-                let p2 = self.next();
-                let o1 = self.next();
-                self.write(o1,self.read(p1) * self.read(p2));
-            },
-            99 => (return),
-            _ => (panic!("Not a viable operator!!")),
-        }
-        self.run();
-    }
-}
-
-
+type Package = (i32,i32,i32);
 
 fn main() {
-    process_input("Day2Input.txt");
+    println!("Test: {}", process_input("Day2test.txt"));
+    println!("Result: {}", process_input("Day2input.txt"));
 }
 
-fn process_input(file: &str) {
+fn process_input(file: &str) -> i64 {
     let file = File::open(file).expect("Bad path");
     let reader = BufReader::new(file);
+    let mut materials: Vec<i32> = Vec::new();
 
-    let vec_prog = reader.lines().nth(0).unwrap().expect("Could not read line 0").split(",").map(|el| el.parse::<i32>().expect("Bad format in program")).collect::<Vec<i32>>();
-    let program = Program::new(vec_prog);
-    println!("Final: {:?}", try_values(&program, 19690720));
-}
-
-fn try_values(program: &Program, look_for: i32) -> i32 {
-    for n in 0..100 {
-        for v in 0..100 {
-            let mut current = program.clone();
-            current.write(1, n);
-            current.write(2, v);
-            current.run();
-            if current.read(0) == look_for {
-                return (100 * n) + v;
-            }
-        }
+    for (index,line) in reader.lines().enumerate() {
+        let line = line.expect(format!("Could not read line {}", index).as_str());
+        materials.push(calculate_package_ribbon(parse_package(line.as_str())));
     }
-    return -1;
+
+    materials.into_iter().fold(0_i64, |acc,el| {
+        acc + el as i64
+    })
 }
+
+fn parse_package(string: &str) -> Package {
+    let str_dims = string.split('x').take(3).collect::<Vec<&str>>();
+    let result: Package = (str_dims[0].parse().expect("Bad number"),str_dims[1].parse().expect("Bad number"),str_dims[2].parse().expect("Bad number"));
+    result
+}
+
+fn calculate_package(package: Package) -> i32 {
+    let s1 = package.0 * package.1;
+    let s2 = package.1 * package.2;
+    let s3 = package.2 * package.0;
+    let extra = min(s1,min(s2,s3));
+    (2*(s1+s2+s3))+extra
+}
+
+fn calculate_package_ribbon(package: Package) -> i32 {
+    let bow = package.0 * package.1 * package.2;
+    let p1 = (package.0 + package.1) * 2;
+    let p2 = (package.1 + package.2) * 2;
+    let p3 = (package.2 + package.1) * 2;
+    let ribbon = min(p1,min(p2,p3));
+    ribbon+bow
+}
+
