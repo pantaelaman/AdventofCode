1601d843c6247b54f4893ba699a674b08c3e6400
pantaelaman <calvin@theophoffs.com>
2023-11-29 15:48:44 -0500

    Day 3 complete

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,16 +1,25 @@
+#![feature(isqrt)]
 fn main() {
-    println!("Part 1: {}", part1(23));
+    let input = std::env::args()
+        .skip(1)
+        .next()
+        .unwrap()
+        .parse::<i32>()
+        .unwrap();
+    println!("Part 1: {}", part1(input));
+    println!("Part 2: https://oeis.org/A141481 & https://oeis.org/A141481/b141481.txt");
 }
 
-fn part1(input: u64) -> u64 {
-    let mut side_length: u64 = 1;
-    let mut max_num: u64 = 1;
-    while max_num < input {
-        side_length += 2;
-        max_num = side_length.pow(2);
-    }
-    let distance = max_num - input;
-    let side = distance / side_length;
-    let offset = distance % side_length;
-    0
+fn part1(input: i32) -> i32 {
+    if input == 1 {
+        return 0;
+    };
+    let layer = ((f32::sqrt(input as f32).ceil() - 1.) / 2.).ceil() as i32;
+    let n = input - (((2 * layer - 1).pow(2)) + 1);
+    let length = layer * 2;
+    let lower = length * (n / length);
+    let upper = lower + length - 1;
+    let mid = (lower + upper - 1) / 2;
+    println!("{}[{}]{}:{}-{}-{}", layer, n, length, lower, mid, upper);
+    layer + (n - mid).abs()
 }
