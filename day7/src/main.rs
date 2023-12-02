6856e94636dfc5dbcb6853cfb403000b36e70999
pantaelaman <calvin@theophoffs.com>
2023-12-01 23:51:02 -0500

    Day 7 incomplete

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -7,8 +7,9 @@ use std::io::{BufRead, BufReader, Read};
 fn main() {
     let file = File::open(std::env::args().skip(1).next().unwrap()).unwrap();
     let mut reader = BufReader::new(file);
-    let (p1, parents, children, weights) = part1(&mut reader);
+    let (p1, _, children, weights) = part1(&mut reader);
     println!("Part 1: {}", p1);
+    println!("Part 2: {}", part2(&p1, &children, &weights));
 }
 
 fn part1<F: Read>(
@@ -17,11 +18,11 @@ fn part1<F: Read>(
     String,
     HashMap<String, String>,
     HashMap<String, Vec<String>>,
-    HashMap<String, usize>,
+    HashMap<String, i32>,
 ) {
     let mut parents: HashMap<String, String> = HashMap::new();
     let mut children: HashMap<String, Vec<String>> = HashMap::new();
-    let mut weights: HashMap<String, usize> = HashMap::new();
+    let mut weights: HashMap<String, i32> = HashMap::new();
 
     let re = Regex::new(r"(\w+) \((\d+)\)(?: -> (.*))?").unwrap();
     for line in reader.lines() {
@@ -38,7 +39,7 @@ fn part1<F: Read>(
             .flatten()
             .unwrap()
             .as_str()
-            .parse::<usize>()
+            .parse::<i32>()
             .unwrap();
         weights.insert(name.to_string(), weight);
         let cur_children = match groups.next().flatten() {
@@ -68,13 +69,45 @@ fn part1<F: Read>(
     )
 }
 
-fn seek_imbalance(
+fn part2(
     root: &String,
     children: &HashMap<String, Vec<String>>,
-    weights: &HashMap<String, usize>,
-) -> Result<usize, usize> {
+    weights: &HashMap<String, i32>,
+) -> i32 {
+    let (name, delta) = seek_imbalance(root, children, weights, 0);
+    println!("imbalanced: {}", name);
+    delta
+}
+
+fn seek_imbalance<'a>(
+    root: &'a String,
+    children: &'a HashMap<String, Vec<String>>,
+    weights: &'a HashMap<String, i32>,
+    delta: i32,
+) -> (&'a String, i32) {
     let root_children = children.get(root).unwrap();
-    let num_children = root_children.len();
-    if num_children == 0 || num_children == 1 {}
-    Err(0)
+    let children_weights = root_children
+        .iter()
+        .map(|c| weights.get(c).unwrap())
+        .collect_vec();
+    // let num_children = root_children.len();
+    // let weight = weights.get(root).unwrap();
+    let mut weight_classes = children_weights
+        .iter()
+        .sorted()
+        .dedup_with_count()
+        .sorted_by(|(a, _), (b, _)| b.cmp(a))
+        .map(|(_, c)| *c);
+    let target = weight_classes.next().unwrap();
+    match weight_classes.next() {
+        Some(w) => {
+            let unbalanced = root_children
+                .iter()
+                .filter(|c| weights.get(*c).unwrap() == w)
+                .exactly_one()
+                .unwrap();
+            seek_imbalance(unbalanced, children, weights, w - target)
+        }
+        None => (root, weights.get(root).unwrap() + delta),
+    }
 }
