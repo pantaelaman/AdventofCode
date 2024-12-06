953d410f3178f17758dde3e0e83d65b4e065c396
pantaelaman <calvin@theophoffs.com>
2024-12-05 22:25:22 -0500

    more concise

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,20 +1,17 @@
 fn main() {
   let input = std::env::args().nth(1).unwrap();
 
-  let mut p1 = 0;
-  let mut i = 1;
-  loop {
+  let mut p1_found = false;
+  for i in 1.. {
     let hash = md5::compute(format!("{}{}", input, i));
     if hash[0] == 0 && hash[1] == 0 {
-      if p1 == 0 && hash[2] <= 0x0f {
-        p1 = i;
-        println!("Part 1: {}", p1);
+      if !p1_found && hash[2] <= 0x0f {
+        p1_found = true;
+        println!("Part 1: {}", i);
       } else if hash[2] == 0 {
+        println!("Part 2: {}", i);
         break;
       }
     }
-    i += 1;
   }
-
-  println!("Part 2: {}", i);
 }
