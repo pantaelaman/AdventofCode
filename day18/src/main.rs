ce940bf78d81bf87d72a0927533d4025c737969f
pantaelaman <calvin@theophoffs.com>
2025-12-11 13:47:50 -0500

    day 18 complete, horrific optimisation but oh well lmao

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,226 +1,292 @@
 use std::{
-  collections::{BTreeMap, BTreeSet, HashMap, HashSet},
-  fs::File,
-  io::{BufRead, BufReader, Write},
-  iter::Successors,
+  cmp,
+  collections::{BinaryHeap, HashMap, HashSet},
+  io::stdin,
 };
 
-use bimap::BiHashMap;
 use itertools::Itertools;
-use pariter::{scope, IteratorExt};
-use pathfinding::prelude::dijkstra;
 
-// don't have to worry about overlap cause of the border
-type Position = (usize, usize);
+#[derive(Hash, PartialEq, Eq, Clone, Copy, Debug)]
+enum Node {
+  Start((i32, i32)),
+  Key(char),
+  Door(char),
+}
+
+enum GridPoint {
+  Node(Node),
+  Wall,
+}
+
+struct Positioned<T> {
+  pos: (usize, usize),
+  data: T,
+}
+
+#[derive(PartialEq, Eq)]
+struct Head {
+  keys: Vec<char>,
+  node: Node,
+  steps: usize,
+}
+
+impl PartialOrd for Head {
+  fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+    self.steps.partial_cmp(&other.steps)
+  }
+}
+
+impl Ord for Head {
+  fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+    self.steps.cmp(&other.steps)
+  }
+}
+
+#[derive(PartialEq, Eq)]
+struct Heads {
+  keys: Vec<char>,
+  nodes: [Node; 4],
+  steps: usize,
+}
+
+impl PartialOrd for Heads {
+  fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+    self.steps.partial_cmp(&other.steps)
+  }
+}
+
+impl Ord for Heads {
+  fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+    self.steps.cmp(&other.steps)
+  }
+}
 
 fn main() {
-  let file = File::open(std::env::args().nth(1).unwrap()).unwrap();
-  let reader = BufReader::new(file);
+  let lines = stdin().lines().map(|line| line.unwrap()).collect_vec();
+  // let contents = lines.join("\n");
 
-  let mut walls = HashSet::new();
-  let mut keys = BiHashMap::new();
-  let mut doors = BiHashMap::new();
-  let mut entrance = (0, 0);
-  for (y, line) in reader.lines().map(|line| line.unwrap()).enumerate() {
-    for (x, c) in line.chars().enumerate() {
-      match c {
-        '.' => continue,
-        '#' => {
-          walls.insert((x, y));
+  let grid = lines
+    .iter()
+    .map(|line| line.chars().collect_vec())
+    .collect_vec();
+
+  let graph = build_graph(&grid);
+
+  let n_keys = graph.keys().filter(|n| matches!(n, Node::Key(_))).count();
+
+  let mut seen: HashSet<(Vec<char>, Node)> = HashSet::new();
+  let mut queue: BinaryHeap<cmp::Reverse<Head>> = BinaryHeap::new();
+  queue.push(cmp::Reverse(Head {
+    keys: Vec::new(),
+    node: *graph.keys().find(|k| matches!(k, Node::Start(_))).unwrap(),
+    steps: 0,
+  }));
+
+  let steps = loop {
+    let Some(cmp::Reverse(head)) = queue.pop() else {
+      panic!("no path found");
+    };
+    if !seen.insert((head.keys.clone(), head.node)) {
+      continue;
+    }
+
+    if head.keys.len() == n_keys {
+      break head.steps;
+    }
+
+    for (neighbour, addl) in graph[&head.node].iter() {
+      match *neighbour {
+        Node::Start(_) => {
+          queue.push(cmp::Reverse(Head {
+            keys: head.keys.clone(),
+            node: *neighbour,
+            steps: head.steps + addl,
+          }));
         }
-        '@' => {
-          entrance = (x, y);
-        }
-        c => {
-          if c.is_ascii_lowercase() {
-            keys.insert((x, y), c);
+        Node::Key(c) => queue.push(cmp::Reverse(Head {
+          keys: if head.keys.contains(&c) {
+            head.keys.clone()
           } else {
-            doors.insert((x, y), c.to_ascii_lowercase());
+            let mut keys = head.keys.clone();
+            keys.push(c);
+            keys.sort_unstable();
+            keys
+          },
+          node: *neighbour,
+          steps: head.steps + addl,
+        })),
+        Node::Door(c) => {
+          if head.keys.contains(&c) {
+            queue.push(cmp::Reverse(Head {
+              keys: head.keys.clone(),
+              node: *neighbour,
+              steps: head.steps + addl,
+            }))
+          } else {
+            continue;
+          }
+        }
+      }
+    }
+  };
+
+  println!("Part 1: {}", steps);
+
+  let grid = {
+    let mut grid = grid;
+    let (sx, sy) = grid
+      .iter()
+      .enumerate()
+      .find_map(|(y, row)| {
+        row
+          .iter()
+          .enumerate()
+          .find_map(|(x, c)| (*c == '@').then_some((x, y)))
+      })
+      .unwrap();
+
+    grid[sy][sx] = '#';
+    grid[sy - 1][sx] = '#';
+    grid[sy + 1][sx] = '#';
+    grid[sy][sx - 1] = '#';
+    grid[sy][sx + 1] = '#';
+    grid[sy + 1][sx + 1] = '@';
+    grid[sy - 1][sx + 1] = '@';
+    grid[sy + 1][sx - 1] = '@';
+    grid[sy - 1][sx - 1] = '@';
+
+    grid
+  };
+
+  let graph = build_graph(&grid);
+
+  let mut seen: HashSet<(Vec<char>, [Node; 4])> = HashSet::new();
+  let mut queue: BinaryHeap<cmp::Reverse<Heads>> = BinaryHeap::new();
+  let starts = graph
+    .keys()
+    .filter(|k| matches!(k, Node::Start(_)))
+    .copied()
+    .collect_vec();
+
+  queue.push(cmp::Reverse(Heads {
+    keys: Vec::new(),
+    nodes: starts[..4].try_into().unwrap(),
+    steps: 0,
+  }));
+
+  let steps = loop {
+    let Some(cmp::Reverse(heads)) = queue.pop() else {
+      panic!("no path found");
+    };
+    if !seen.insert((heads.keys.clone(), heads.nodes)) {
+      continue;
+    }
+
+    if heads.keys.len() == n_keys {
+      break heads.steps;
+    }
+
+    for (i, node) in heads.nodes.iter().copied().enumerate() {
+      for (neighbour, addl) in graph[&node].iter() {
+        let next_nodes = {
+          let mut next = heads.nodes.clone();
+          next[i] = *neighbour;
+          next
+        };
+
+        match *neighbour {
+          Node::Start(_) => {
+            queue.push(cmp::Reverse(Heads {
+              keys: heads.keys.clone(),
+              nodes: next_nodes,
+              steps: heads.steps + addl,
+            }));
+          }
+          Node::Key(c) => queue.push(cmp::Reverse(Heads {
+            keys: if heads.keys.contains(&c) {
+              heads.keys.clone()
+            } else {
+              let mut keys = heads.keys.clone();
+              keys.push(c);
+              keys.sort_unstable();
+              keys
+            },
+            nodes: next_nodes,
+            steps: heads.steps + addl,
+          })),
+          Node::Door(c) => {
+            if heads.keys.contains(&c) {
+              queue.push(cmp::Reverse(Heads {
+                keys: heads.keys.clone(),
+                nodes: next_nodes,
+                steps: heads.steps + addl,
+              }))
+            } else {
+              continue;
+            }
+          }
+        }
+      }
+    }
+  };
+
+  println!("Part 2: {steps}");
+}
+
+fn build_graph(grid: &Vec<Vec<char>>) -> HashMap<Node, Vec<(Node, usize)>> {
+  let nodes: HashMap<(i32, i32), GridPoint> = grid
+    .iter()
+    .enumerate()
+    .flat_map(|(y, row)| {
+      row.iter().enumerate().filter_map(move |(x, c)| {
+        let xy = (x as i32, y as i32);
+        if *c == '@' {
+          Some((xy, GridPoint::Node(Node::Start(xy))))
+        } else if *c == '#' {
+          Some((xy, GridPoint::Wall))
+        } else if c.is_ascii_uppercase() {
+          Some((xy, GridPoint::Node(Node::Door(c.to_ascii_lowercase()))))
+        } else if c.is_ascii_lowercase() {
+          Some((xy, GridPoint::Node(Node::Key(*c))))
+        } else {
+          None
+        }
+      })
+    })
+    .collect();
+
+  let mut graph: HashMap<Node, Vec<(Node, usize)>> = HashMap::new();
+
+  for (xy, node) in nodes.iter().filter_map(|(xy, point)| match point {
+    GridPoint::Node(node) => Some((xy, node)),
+    _ => None,
+  }) {
+    let mut frontier = vec![(*xy, 0)];
+    let mut seen = HashSet::new();
+    while !frontier.is_empty() {
+      for ((x, y), steps) in std::mem::take(&mut frontier) {
+        let steps = steps + 1;
+        seen.insert((x, y));
+
+        let neighbours = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)];
+        for neighbour in neighbours {
+          if seen.contains(&neighbour) {
+            continue;
+          }
+
+          match nodes.get(&neighbour) {
+            Some(GridPoint::Wall) => {}
+            Some(GridPoint::Node(next)) => {
+              graph.entry(*node).or_default().push((*next, steps));
+            }
+            None => {
+              frontier.push((neighbour, steps));
+            }
           }
         }
       }
     }
   }
 
-  //println!("Walls: {walls:?}");
-  //println!("Keys: {keys:?}");
-  //println!("Doors: {doors:?}");
-  //println!("Entrance: {entrance:?}");
-
-  //  let mut frontier = vec![entrance];
-  //  let mut visited = HashSet::new();
-  //  let mut steps = 0;
-  //  'bfs: while !keys.is_empty() {
-  //    for (x, y) in std::mem::take(&mut frontier) {
-  //      if let Some(key) = keys.remove(&(x, y)) {
-  //        visited.clear();
-  //        frontier.clear();
-  //        frontier.push((x, y));
-  //        doors.remove_by_right(&key.to_ascii_uppercase());
-  //        continue 'bfs;
-  //      }
-  //      visited.insert((x, y));
-  //      let successors = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
-  //        .into_iter()
-  //        // .inspect(|p| println!("{p:?}"))
-  //        .filter(|p| {
-  //          !visited.contains(p) && !walls.contains(p) && !doors.contains_left(p)
-  //        });
-  //      // .inspect(|p| println!("{p:?} made it"));
-  //      frontier.extend(successors);
-  //    }
-  //    steps += 1;
-  //  }
-  keys.insert(entrance, '@');
-  let keys_to_doors = map_passed_doors(entrance, &walls, &keys, &doors);
-  println!("{:?}", keys_to_doors);
-  let key_dists = calc_key_distances(&walls, &keys);
-  println!("{:?}", key_dists);
-  //let steps = reachable_keys(entrance, &walls, &keys, &doors, HashSet::new());
-  let steps =
-    collapse_dists_and_doors(entrance, &keys, &keys_to_doors, &key_dists);
-
-  println!("Part 1: {}", steps);
-}
-
-fn collapse_dists_and_doors(
-  start: Position,
-  keys: &BiHashMap<Position, char>,
-  keys_to_doors: &HashMap<char, HashSet<char>>,
-  key_dists: &HashMap<char, HashMap<char, usize>>,
-) -> usize {
-  #[derive(Hash, Debug, PartialEq, Eq, Clone)]
-  struct Node {
-    position: Position,
-    collected: BTreeSet<char>,
-  }
-
-  let mut frontier: BTreeMap<usize, Vec<Node>> = BTreeMap::new();
-  let mut init_set = BTreeSet::new();
-  init_set.insert('@');
-  frontier.insert(
-    0,
-    vec![Node {
-      position: start,
-      collected: init_set,
-    }],
-  );
-
-  let mut seen: HashSet<Node> = HashSet::new();
-
-  loop {
-    //println!("{frontier:?}");
-    let (prior_dist, nodes) = frontier.first_entry().unwrap().remove_entry();
-    for node in nodes {
-      if !seen.insert(node.clone()) {
-        //println!("Skipped!");
-        continue;
-      }
-
-      if node.collected.len() == keys.len() {
-        return prior_dist;
-      }
-      //println!("{:?} ({:?})", node.position, node.collected);
-      let key = keys.get_by_left(&node.position).unwrap();
-      //println!("{key:?}");
-      let local_dists = key_dists.get(key).unwrap();
-      //println!("{local_dists:?}");
-      for (new_key, dist) in local_dists.iter().filter(|(new_key, _)| {
-        !node.collected.contains(new_key)
-          && keys_to_doors
-            .get(new_key)
-            .unwrap()
-            .iter()
-            .all(|d| node.collected.contains(d))
-      }) {
-        //println!("Next: {new_key:?}, ({dist})");
-        let mut new_collected = node.collected.clone();
-        new_collected.insert(*new_key);
-        frontier.entry(dist + prior_dist).or_default().push(Node {
-          position: *keys.get_by_right(new_key).unwrap(),
-          collected: new_collected,
-        });
-      }
-      //println!();
-    }
-  }
-}
-
-fn map_passed_doors(
-  start: Position,
-  walls: &HashSet<Position>,
-  keys: &BiHashMap<Position, char>,
-  doors: &BiHashMap<Position, char>,
-) -> HashMap<char, HashSet<char>> {
-  let mut frontier = vec![(start, HashSet::<char>::new())];
-  let mut visited = HashSet::new();
-  let mut keys_to_doors = HashMap::new();
-  while !frontier.is_empty() {
-    for ((x, y), mut passed_doors) in std::mem::take(&mut frontier) {
-      visited.insert((x, y));
-      if let Some(key) = keys.get_by_left(&(x, y)) {
-        keys_to_doors.insert(*key, passed_doors.clone());
-        passed_doors.insert(*key); // keys are doors too!
-      } else if let Some(door) = doors.get_by_left(&(x, y)) {
-        passed_doors.insert(*door);
-      }
-      let successors = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
-        .into_iter()
-        .filter(|p| !visited.contains(p) && !walls.contains(p))
-        .map(|s| (s, passed_doors.clone()));
-      frontier.extend(successors);
-    }
-  }
-  keys_to_doors
-}
-
-fn calc_key_distances(
-  walls: &HashSet<Position>,
-  keys: &BiHashMap<Position, char>,
-) -> HashMap<char, HashMap<char, usize>> {
-  let mut key_distances: HashMap<char, HashMap<char, usize>> = HashMap::new();
-  //println!("\n--- Dists ---");
-  for ((p_a, key_a), (p_b, key_b)) in keys.iter().tuple_combinations() {
-    //println!("{key_a} <-> {key_b}");
-    let dist = path_length(*p_a, *p_b, walls).unwrap();
-    //println!("{dist}");
-    key_distances
-      .entry(*key_a)
-      .or_default()
-      .insert(*key_b, dist);
-    key_distances
-      .entry(*key_b)
-      .or_default()
-      .insert(*key_a, dist);
-    //println!();
-  }
-  //println!("\n--- End ---");
-  key_distances
-}
-
-fn path_length(
-  start: Position,
-  target: Position,
-  walls: &HashSet<Position>,
-) -> Option<usize> {
-  let mut frontier = vec![start];
-  let mut visited = HashSet::new();
-  let mut steps = 0;
-  while !frontier.is_empty() {
-    for (x, y) in std::mem::take(&mut frontier) {
-      if (x, y) == target {
-        return Some(steps);
-      }
-      visited.insert((x, y));
-      let successors = raw_successors((x, y))
-        .filter(|p| !visited.contains(p) && !walls.contains(p));
-      frontier.extend(successors);
-    }
-    steps += 1;
-  }
-  None
-}
-
-fn raw_successors((x, y): Position) -> impl Iterator<Item = Position> {
-  [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)].into_iter()
+  graph
 }
